0bcb5afc862f4b09c702880cc070d3c40ef713c0
Lucas Woibau <93388159+Lucas-Woibau@users.noreply.github.com>
2024-09-05 14:47:22 -0300

    Criando CRUD completo com modals

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CampusConnect.Controllers
 {
@@ -38,6 +39,105 @@ namespace CampusConnect.Controllers
             ViewBag.TotalPages = totalPages;
 
             return View(users);
+        }
+
+        public async Task<IActionResult> Details(string? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index", "Users");
+            }
+
+            var appUser = await _userManager.FindByIdAsync(id);
+
+            if (appUser == null)
+            {
+                return RedirectToAction("Index", "Users");
+            }
+
+            ViewBag.Roles = await _userManager.GetRolesAsync(appUser);
+
+            // Get available roles
+            var availableRoles = _roleManager.Roles.ToList();
+            var items = new List<SelectListItem>();
+            foreach (var role in availableRoles)
+            {
+                items.Add(
+                    new SelectListItem
+                    {
+                        Text = role.NormalizedName,
+                        Value = role.Name,
+                        Selected = await _userManager.IsInRoleAsync(appUser, role.Name!),
+                    });
+            }
+
+            ViewBag.SelectItems = items;
+
+            return View(appUser);
+        }
+
+        public async Task<IActionResult> EditRole(string? id, string? newRole)
+        {
+            if (id == null || newRole == null)
+            {
+                return RedirectToAction("Index", "Users");
+            }
+
+            var roleExists = await _roleManager.RoleExistsAsync(newRole);
+            var appUser = await _userManager.FindByIdAsync(id);
+
+            if (appUser == null || !roleExists)
+            {
+                return RedirectToAction("Index", "Users");
+            }
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser!.Id == appUser.Id)
+            {
+                TempData["ErrorMessage"] = "You cannot update your own role!";
+                return RedirectToAction("Details", "Users", new { id });
+            }
+
+            // Update user role
+            var userRoles = await _userManager.GetRolesAsync(appUser);
+            await _userManager.RemoveFromRolesAsync(appUser, userRoles);
+            await _userManager.AddToRoleAsync(appUser, newRole);
+
+            TempData["SuccessMessage"] = "User Role updated successfully";
+            return RedirectToAction("Details", "Users", new { id });
+        }
+
+        public async Task<IActionResult> DeleteAccount(string? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index", "Users");
+            }
+
+            var appUser = await _userManager.FindByIdAsync(id);
+
+            if (appUser == null)
+            {
+                return RedirectToAction("Index", "Users");
+            }
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser!.Id == appUser.Id)
+            {
+                TempData["ErrorMessage"] = "You cannot delete your own role!";
+                return RedirectToAction("Details", "Users", new { id });
+            }
+
+            // Delete
+            var result = await _userManager.DeleteAsync(appUser);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Users");
+            }
+
+            TempData["ErrorMessage"] = "Unable to delete this account: " + result.Errors.First().Description;
+
+            return RedirectToAction("Details", "Users", new { id });
         }
     }
 }
